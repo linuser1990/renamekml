8bbd696fe09eda27fdb5e12c11d449f0b2e0005b
linuser1990 <linuxserver@hotmail.com>
2023-10-06 16:25:18 -0400

    renomeia apenas postes e deixa distancias nomeadas

--- index.js
+++ index.js
@@ -12,30 +12,45 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
   
+try{
 
+  console.log('chegou aqui');
       // Load the KML file
-const kmlData = fs.readFileSync('./rename.kml', 'utf8');
+const kmlData = fs.readFileSync('./file.kml', 'utf8');
 const parser = new DOMParser();
 const xmlDoc = parser.parseFromString(kmlData, 'text/xml');
 
 // Access the KML document and iterate over the placemarks
 const placemarks = xmlDoc.getElementsByTagName('Placemark');
 
+console.log('chegou');
+
 for (let i = 0; i < placemarks.length; i++) {
   const placemark = placemarks[i];
 
   // Rename the placemark
   const nameElement = placemark.getElementsByTagName('name')[0];
-  nameElement.textContent = ' ';
+  const name = nameElement.textContent;
+  
+  // Check if the name contains "kml" (case insensitive)
+  if (name.toLowerCase().includes('kml')) {
+    // Rename the placemark
+    nameElement.textContent = ' ';
+  }
+
+  
 } 
 
 // Save the modified KML data to a new file
 const serializer = new XMLSerializer();
 const modifiedKmlData = serializer.serializeToString(xmlDoc);
-fs.writeFileSync('./output.kml', modifiedKmlData, 'utf8');
+fs.writeFileSync('./output3.kml', modifiedKmlData, 'utf8');
 
 
 console.log('entrou');
-
+}catch(error)
+{
+  console.log(error)
+}
 
 });
